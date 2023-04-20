9e093bf3e16ad4bbce1550028aa1b4d5090bef5a
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-04-20 22:46:06 +0100

    started work on start-game event

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Query Selector's
+
+var startGameButton = document.querySelector("#start-game")
+var 
+
+
+// Global Variables
+
+function startGame(event) {
+
+    console.log(startGameButton)
+    console.log("test")
+}
+
